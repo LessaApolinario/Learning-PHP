9b5a974d4c0649035b71e4a6d66a5097b4bb18be
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-05 20:30:20 -0300

    short_open_tag

--- index.php
+++ index.php
@@ -10,6 +10,21 @@
     echo 'Utiizando a tag padrão';
   ?>
   <br />
+  <!--
+    Se for apenas uma instrução por tag,
+    o ponto e vírgula é opcional.
+  -->
   <?= 'Utiizando a tag impressão' ?>
+  <br />
+  <!--?
+    Descontinuada no PHP 8.
+    'Utilizando a tag curta'
+    mas pode ser habilitada no arquivo php.ini,
+    para tanto devemos procurar pela configuração
+    "shot_open_tag" teclando "Ctrl + F" no bloco de notas.
+    No shot_open_tag=Off configuramos de Off para On.
+  ?-->
+  <!-- "short_open_tag=On" -->
+  <? echo 'Utilizando a tag curta, agora funcionando' ?>
 </body>
 </html>
