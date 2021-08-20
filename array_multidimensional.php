b417813c4db928d3cbbe4f58f41111abdb9fb10f
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-19 22:12:21 -0300

    Array multidimensional

---
+++ array_multidimensional.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <?php
+    // $lista_coisas = array();
+    $lista_coisas = [];
+
+    $lista_coisas["frutas"] = array(
+      1 => "Banana",
+      2 => "Maçã",
+      3 => "Morango",
+      4 => "Uva"
+    );
+
+    $lista_coisas["pessoas"] = [
+      1 => "João",
+      2 => "José",
+      3 => "Maria"
+    ];
+
+    echo "<pre>";
+      print_r($lista_coisas);
+    echo "<pre/>";
+
+    echo "<hr />";
+    echo $lista_coisas["frutas"][3]; // Morango
+    echo "<br />";
+    echo $lista_coisas["pessoas"][2]; // José
+  ?>
+</body>
+</html>
