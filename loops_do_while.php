13348045195805dbf7c21d39859b37ecf80c8fb3
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-21 15:08:57 -0300

    do while

---
+++ loops_do_while.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <?php
+    $x = 1;
+
+    do {
+      echo "X = $x<br />";
+
+      $x++;
+    } while($x < 9);
+  ?>
+</body>
+</html>
